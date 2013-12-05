825e7c396bdf2dcf70c5bf5a299489058a06e912
Roberto Ferranti <roberto.ferranti@gmail.com>
2013-12-05 22:45:45 +0100

    fix: muppet_auth_always was not supposed to challeng, ever

--- apps/muppet_auth/src/muppet_auth_always.erl
+++ apps/muppet_auth/src/muppet_auth_always.erl
@@ -4,11 +4,15 @@
 
 init(_Any) ->
     [].
+    
 is_authorized(_State, _Req) ->
     true.
+
 can_challenge(_State, _Req) ->
-    true.
+    false.
+
 challenge(_State, _Req) ->
-    [].
+    throw(muppet_auth_always_never_challenges).
+
 terminate(_State) ->
     ok.
