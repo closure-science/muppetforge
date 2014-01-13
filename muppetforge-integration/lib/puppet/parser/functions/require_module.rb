c725bd72f4479d1ae11eb54a624059a8ba81ccc7
Tsukihara Caligin <caligin35@gmail.com>
2014-01-13 15:19:06 +0100

    fix: use PMT upgrader instead of installer with --force as it skips dependency resolution; enh: bumped muppetforge/integration to next SNAPSHOT

--- muppetforge-integration/lib/puppet/parser/functions/require_module.rb
+++ muppetforge-integration/lib/puppet/parser/functions/require_module.rb
@@ -1,10 +1,15 @@
+def module_exists(node, author, name)
+    node.modules.select do |mod|
+        (mod.name == name && mod.author == author)
+    end.empty? == false
+end
+
 module Puppet::Parser::Functions
     newfunction(:require_module, :type => :statement, :arity => 3) do | args |
         author, modulename, version = args
         env_name = Puppet.settings[:environment]
         modulepath = Puppet.settings.value(:modulepath, env_name)
         options = {
-            :force   => true,
             :version => version,
             :environment => env_name,
             :modulepath  => modulepath,
@@ -13,9 +18,13 @@ module Puppet::Parser::Functions
         fn_version = SemVer.new "0.0.1" #whatever
         forge = Puppet::Forge.new("Muppetforge", fn_version)
         install_dir = Puppet::ModuleTool::InstallDirectory.new(Pathname.new(options[:target_dir]))
-        installer = Puppet::ModuleTool::Applications::Installer.new("#{author}/#{modulename}", forge, install_dir, options)
-        installer.run
         env = Puppet::Node::Environment.current
+        if module_exists(env, author, modulename) then 
+            installer = Puppet::ModuleTool::Applications::Upgrader.new("#{author}/#{modulename}", forge, options)
+        else
+            installer = Puppet::ModuleTool::Applications::Installer.new("#{author}/#{modulename}", forge, install_dir, options)
+        end
+        installer.run
         env.instance_eval do ||
             @attr_expirations[:modules] = Time.now
         end
